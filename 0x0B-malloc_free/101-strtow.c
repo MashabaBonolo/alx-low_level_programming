7a6d4e22f64e1a0d688dc1692310749843883d51
MashabaBonolo <MashabaBonolo29@gmail.com>
2023-07-13 15:05:29 -0700

    all task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,131 +1,77 @@
+#include <stdlib.h>
 #include "main.h"
-#include <string.h>
-
-int getWordsCount(char *str);
-void getWordCoordinates(char *str, int startFrom, int *start, int *end);
 
 /**
- * strtow - Function that splits a string into words.
- * @str: String
- * Return: Array of words
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ *
+ * Return: number of words
+ */
+int count_word(char *s)
+{
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
  */
 char **strtow(char *str)
 {
-	int i, j, wordLength, numWords, wordIndex = 0, start = 0, end = 0;
-	char **words;
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-	if (str == NULL || *str == '\0' || str[0] == '\0')
-	{
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
 		return (NULL);
-	}
 
-	while (*str != '\0' && (*str == ' ' || *str == '\t' || *str == '\n'))
-	{
-		str++;
-	}
-
-	if (*str == '\0')
-	{
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
 		return (NULL);
-	}
 
-	numWords = getWordsCount(str);
-	words = malloc((numWords + 1) * sizeof(char *));
-
-	if (words == NULL)
+	for (i = 0; i <= len; i++)
 	{
-		return (NULL);
-	}
-
-	for (i = 0; i < numWords; i++)
-	{
-		getWordCoordinates(str, start, &start, &end);
-		wordLength = end - start + 1;
-
-		words[wordIndex] = malloc((wordLength + 1) * sizeof(char));
-
-		if (words[wordIndex] == NULL)
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-
-			for (j = 0; j < wordIndex; j++)
+			if (c)
 			{
-				free(words[j]);
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
 			}
-			free(words);
-			return (NULL);
 		}
-
-		strncpy(words[wordIndex], &str[start], wordLength);
-		words[wordIndex][wordLength] = '\0';
-
-		start = end + 1;
-		wordIndex++;
+		else if (c++ == 0)
+			start = i;
 	}
 
-	words[wordIndex] = NULL;
+	matrix[k] = NULL;
 
-	return (words);
-}
-
-/**
- * getWordsCount - Function that splits a string into words.
- * @str: String
- * Return: words count
- */
-int getWordsCount(char *str)
-{
-	int count = 0;
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
-		{
-			count++;
-			while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i] != '\0')
-			{
-				i++;
-			}
-		} else
-		{
-			i++;
-		}
-	}
-
-	return (count);
-}
-
-/**
- * getWordCoordinates - Function that splits a string into words.
- * @str: String
- * @startFrom: start from
- * @start: word start
- * @end: word end
- * Return: Nothing
- */
-void getWordCoordinates(char *str, int startFrom, int *start, int *end)
-{
-	int index = startFrom;
-	int flag = 0;
-
-	while (str[index] != '\0')
-	{
-		if (str[index] != ' ' && str[index] != '\t'
-				&& str[index] != '\n' && flag == 0)
-		{
-			*start = index;
-			flag = 1;
-		} else if ((str[index] == ' ' || str[index] == '\t'
-					|| str[index] == '\n') && flag == 1)
-		{
-			*end = index - 1;
-			break;
-		}
-		index++;
-	}
-
-	if (flag == 1 && str[index] == '\0')
-	{
-		*end = index - 1;
-	}
+	return (matrix);
 }
